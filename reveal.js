11a55e0e2e8d0ea25c9943856397db543224e3cd
Flavianna Saldanha <flaviannasaldanha@gmail.com>
2024-11-05 21:50:27 -0300

    projeto finalizado!

---
+++ reveal.js
@@ -0,0 +1,24 @@
+window.revelar = ScrollReveal({reset:true})
+
+revelar.reveal('.container',
+{
+        duration: 900,
+        distance: '90px'
+})
+
+revelar.reveal('.desc-img',
+{
+        duration: 1000,
+        distance: '90px',
+        delay: 200
+})
+
+revelar.reveal('.beneficios p',
+{
+        duration: 500,
+        distance: '90px',
+        delay: 100,
+        origin: 'left'
+})
+
+    
